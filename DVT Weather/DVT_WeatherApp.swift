58f0259161e4d5784fc391b9eeadb8a472ae3685
bedan <bedank6@gmail.com>
2025-01-10 12:36:14 +0300

    Initial Commit

---
+++ DVT Weather/DVT_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVT_WeatherApp.swift
+//  DVT Weather
+//
+//  Created by Bedan Kimani on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DVT_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
